6f7af37407a1c1f78f69e7dfc176847da5d0023f
Abe885 <abrahamgc1901@gmail.com>
2024-08-09 16:51:20 -0700

    Created RoleRepository and adjusted UserService

---
+++ src/main/java/com/coderscampus/studysync/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.coderscampus.studysync.repository;
+
+import com.coderscampus.studysync.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+
+}
--- src/main/java/com/coderscampus/studysync/service/UserService.java
+++ src/main/java/com/coderscampus/studysync/service/UserService.java
@@ -1,23 +1,33 @@
 package com.coderscampus.studysync.service;
 
+import com.coderscampus.studysync.domain.Role;
 import com.coderscampus.studysync.domain.User;
+import com.coderscampus.studysync.repository.RoleRepository;
 import com.coderscampus.studysync.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
 
     public void saveUser(User user) {
+        Role userRole = roleRepository.findByName("USER");
+        user.setRoles(Collections.singletonList(userRole));
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        
         userRepository.save(user);
     }
 }
